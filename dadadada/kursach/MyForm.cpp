72d2ac4426c57e23a0452c871f7ada30bed84be0
tytmyt <130250967+tytmyt@users.noreply.github.com>
2024-05-20 21:07:41 +0300

    Add files via upload

---
+++ dadadada/kursach/MyForm.cpp
@@ -0,0 +1,12 @@
+﻿#include "MyForm.h"
+[STAThread]
+int main(array<String^>^ args) {
+   
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    // Çàïóñê ôîðìè
+    Application::Run(gcnew MainForm());
+
+    return 0;
+}
